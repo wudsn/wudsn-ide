a77f5cf51ce006a864107f074d2f56f729e037a7
peterdell <jac@wudsn.com>
2018-12-30 16:27:49 +0100

    Add hex editor plugin

---
+++ com.wudsn.ide.hex/src/com/wudsn/ide/hex/HexPlugin.java
@@ -0,0 +1,52 @@
+package com.wudsn.ide.hex;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class HexPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.wudsn.ide.hex"; //$NON-NLS-1$
+
+	// The shared instance
+	private static HexPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public HexPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static HexPlugin getInstance() {
+		return plugin;
+	}
+
+}
