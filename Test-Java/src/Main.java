8134a83abfce25f1b376f4e80852c21a174d73d6
peterdell <jac@wudsn.com>
2018-12-30 15:47:13 +0100

    Test

---
+++ Test-Java/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.commons.compress.archivers.zip.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+	String file = "C:\\Users\\D025328\\Documents\\Eclipse\\workspace.jac\\com.wudsn.site\\site\\productions\\atari800\\thecartstudio\\thecartstudio.zip";
+	System.out.println(file);
+	ZipFile zipFile=new ZipFile(new File(file));
+	ZipEntry entry=zipFile.getEntry("TheCartStudio.jar");
+	System.out.println(entry+":"+ entry.getSize());
+	ZipArchiveEntry archiveEntry=new ZipArchiveEntry(entry);
+	System.out.println(archiveEntry+":"+ archiveEntry.getUnixMode());
+	zipFile.close();
+    }
+
+}
