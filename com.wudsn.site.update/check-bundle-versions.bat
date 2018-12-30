3ab03839a8df094cb138eb1be6a5ff26c6904202
peterdell <jac@wudsn.com>
2018-12-30 22:01:35 +0100

    Initial commit

---
+++ com.wudsn.site.update/check-bundle-versions.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+set PATH=%PATH%;C:\jac\bin\wbin
+cd C:\jac\system\Java\Programming\Workspaces\WUDSN-IDE
+
+call :check com.wudsn.ide.asm
+call :check com.wudsn.ide.asm.compilers
+call :check com.wudsn.ide.asm.compilers.test
+call :check com.wudsn.ide.base
+call :check com.wudsn.ide.dsk
+call :check com.wudsn.ide.gfx
+call :check com.wudsn.ide.snd
+
+call :check com.wudsn.ide.base.feature
+call :check com.wudsn.ide.feature
+pause
+goto :eof
+
+
+:check
+rem echo | set /p=%1	
+echo %1
+if exist %1\META-INF\MANIFEST.MF grep Bundle-Version %1\META-INF\MANIFEST.MF
+if exist %1\META-INF\MANIFEST.MF grep Bundle-RequiredExecutionEnvironment %1\META-INF\MANIFEST.MF
+if exist %1\build.properties     grep jre.compilation.profile %1\build.properties
+if exist %1\feature.xml          grep version= %1\feature.xml
+echo.
+goto :eof
