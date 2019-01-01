0b3b3177dad2c3b2a800782d0e2afddafb81bfb1
peterdell <jac@wudsn.com>
2019-01-01 18:12:38 +0100

    Update help, releases and videos to 1.6.6

--- com.wudsn.ide.asm/help/create-help-links.bat
+++ com.wudsn.ide.asm/help/create-help-links.bat
@@ -15,7 +15,7 @@ set REPOSITORY=C:\jac\system\Java\Programming\Repositories\WUDSN-IDE
 set SITE=C:\jac\system\WWW\Sites\www.wudsn.com
 set SYMBOLIC_LINK=%REPOSITORY%\com.wudsn.ide.asm\help\productions\java\ide
 set REAL_FOLDER=%SITE%\productions\java\ide
-echo on
+
 rmdir %SYMBOLIC_LINK%
 mklink /D %SYMBOLIC_LINK% %REAL_FOLDER%
 pause
