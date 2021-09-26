2f9bf2051cebd979e882d05f94a1d3d0da7601c6
peterdell <jac@wudsn.com>
2021-09-26 22:57:39 +0200

    First commit of new com.wudsn.ide.lng.run plugin

---
+++ com.wudsn.ide.lng.run/src/com/wudsn/ide/lng/run/atari8/Atari800Win.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2009 - 2021 <a href="https://www.wudsn.com" target="_top">Peter Dell</a>
+ *
+ * This file is part of WUDSN IDE.
+ * 
+ * WUDSN IDE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * WUDSN IDE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with WUDSN IDE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.wudsn.ide.lng.run.atari8;
+
+import com.wudsn.ide.lng.runner.Runner;
+
+/**
+ * Runner for Atar800Win.
+ * 
+ * @author Peter Dell
+ * @since 1.7.0.
+ */
+public final class Atari800Win extends Runner {
+
+}
