fb2baa3290855e148ed4f60aa86c65fa9cf81f0e
Peter Dell <peter.dell@web.de>
2022-12-26 04:02:58 +0100

    Create junction links for help instead of symlinks

--- com.wudsn.ide.lng/help/create-help-links.bat
+++ com.wudsn.ide.lng/help/create-help-links.bat
@@ -5,17 +5,14 @@ rem The are linked into the "com.wudsn.ide.lng/help/productions/java/ide project
 rem This way, the Eclipse build uses the latest versions automatically.
 rem The HTML files for the online help are original in the com.wudsn.ide.lng/help" folder.
 rem They have to be copied into the Joomla using the "export.bat" script when something is changed.
-rem 
-rem Important: Run this script in an Administrator shell.
 rem
 
-echo on
 setlocal
 set REPOSITORY=C:\jac\system\Java\Programming\Repositories\WUDSN-IDE
 set SITE=C:\jac\system\WWW\Sites\www.wudsn.com
 set SYMBOLIC_LINK=%REPOSITORY%\com.wudsn.ide.lng\help\productions\java\ide
 set REAL_FOLDER=%SITE%\productions\java\ide
 
-rmdir %SYMBOLIC_LINK%
-mklink /D %SYMBOLIC_LINK% %REAL_FOLDER%
+if exist %SYMBOLIC_LINK% rmdir %SYMBOLIC_LINK%
+mklink /J %SYMBOLIC_LINK% %REAL_FOLDER%
 pause
