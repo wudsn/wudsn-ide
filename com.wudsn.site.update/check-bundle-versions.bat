b6048a0ad338d70adb8e240a4aac91940296a2d1
Peter Dell <peter.dell@web.de>
2022-12-26 02:18:54 +0100

    Rework site update scripts

--- com.wudsn.site.update/check-bundle-versions.bat
+++ com.wudsn.site.update/check-bundle-versions.bat
@@ -1,16 +1,16 @@
 @echo off
 setlocal
 set PATH=%PATH%;C:\jac\bin\wbin
-cd C:\jac\system\Java\Programming\Repositories\WUDSN-IDE
+set FOLDER=C:\jac\system\Java\Programming\Repositories\WUDSN-IDE
+cd %FOLDER%
 
+echo Checking bundle versions in %FOLDER%.
 call :check com.wudsn.ide.base
 call :check com.wudsn.ide.hex
 call :check com.wudsn.ide.dsk
 call :check com.wudsn.ide.gfx
 call :check com.wudsn.ide.lng
 call :check com.wudsn.ide.lng.asm
-call :check com.wudsn.ide.lng.asm.compilers
-call :check com.wudsn.ide.lng.asm.compilers.test
 call :check com.wudsn.ide.lng.pas
 call :check com.wudsn.ide.snd
 
@@ -21,11 +21,10 @@ goto :eof
 
 
 :check
-rem echo | set /p=%1	
-echo %1
+echo Plugin: %1
 if exist %1\META-INF\MANIFEST.MF grep Bundle-Version %1\META-INF\MANIFEST.MF
 if exist %1\META-INF\MANIFEST.MF grep Bundle-RequiredExecutionEnvironment %1\META-INF\MANIFEST.MF
 if exist %1\build.properties     grep jre.compilation.profile %1\build.properties
-if exist %1\feature.xml          grep version= %1\feature.xml
+if exist %1\feature.xml          grep version= %1\feature.xml|grep qualifier
 echo.
 goto :eof
